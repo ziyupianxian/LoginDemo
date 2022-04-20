637052c47397899b8076d0ff4a3707640573382c
dongdi <1745107464@qq.com>
2022-04-20 19:44:37 +0800

    I changed it.

--- main.cpp
+++ main.cpp
@@ -4,6 +4,6 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	printf("This is an experiment.\n");
+	printf("This is an experiment.It's changed.'\n");
 	return 0;
 }
