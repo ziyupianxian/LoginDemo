bb5b3d87ca103848e9bd2f3aef512dbc1ad95a9a
董娣 <87884503+ziyupianxian@users.noreply.github.com>
2022-04-20 20:03:49 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,6 +4,6 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	printf("This is an experiment.It's changed.'\n");
+	printf("This is an experiment.It's changed.New changed in the github.com'\n");
 	return 0;
 }
